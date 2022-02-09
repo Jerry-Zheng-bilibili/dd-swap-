22b52918142964f77e4fb4bb2102ce2097eabafc
Martin-Winfred <blakewinfred@gmail.com>
2022-02-09 14:07:55 +0800

    Add custermize blocksize function

--- ddswap.sh
+++ ddswap.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-dd if=/dev/zero of=/mnt/swap bs=1M count=2048
+# auth Martin-Winfred
+if [ $EUID -ne 0 ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo -e "Input how many swap you want to make (MB)\n Eg.2048"
+echo "Worning Do not use oversized space"
+read coun
+dd if=/dev/zero of=/mnt/swap bs=1M count=$coun
+
 mkswap /mnt/swap
 swapon /mnt/swap
 echo '/mnt/swap swap swap defaults 0 0' >> /etc/fstab
